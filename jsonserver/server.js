a7413179d39358f67027690f921664472c9a3989
harshita <harshita@logicrays.com>
2024-07-10 11:03:49 +0530

    10jul

--- jsonserver/server.js
+++ jsonserver/server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const path =require('path')
 const server = jsonServer.create();
 const fs = require('fs')
 const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
