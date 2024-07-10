9f5879ea5fd321cffd7c745e9332caeef1987271
harshita <harshita@logicrays.com>
2024-07-10 11:01:06 +0530

    10jul

--- jsonserver/server.js
+++ jsonserver/server.js
@@ -1,6 +1,8 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')))
+const router = jsonServer.router(db)
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
