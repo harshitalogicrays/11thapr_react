f21c8d3aa31a9c5b4851524f3e73064c33ba3ac3
harshita <harshita@logicrays.com>
2024-07-16 09:18:20 +0530

    16jul

--- backendjson/server.js
+++ backendjson/server.js
@@ -1,15 +1,15 @@
-import jsonServer from 'json-server'
-import fs from 'fs'
-import path from 'path'
+const jsonServer = require('json-server');
+const path =require('path')
+const server = jsonServer.create();
+const fs = require('fs')
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')))
+const router = jsonServer.router(db)
+const middlewares = jsonServer.defaults();
 
-const server = jsonServer.create()
-const __dirname = path.resolve();
-// const db = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json')))
-const router = jsonServer.router(path.join(__dirname, 'data.json'))
-const middlewares = jsonServer.defaults()
+server.use(middlewares);
+server.use(router);
 
-server.use(middlewares)
-server.use(router)
-server.listen(1000, () => {
-  console.log('JSON Server is running')
-})
+const port = 8080;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
